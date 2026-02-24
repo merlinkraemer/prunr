c62fd574d5d35d578f289b1afc59b103c76e0590
Merlin Krämer <merlinkraemer@gmail.com>
2026-02-24 16:49:36 +0700

    feat: expand common paths with dev cache and tooling dirs

--- Prunr/Models/TrackedPath.swift
+++ Prunr/Models/TrackedPath.swift
@@ -36,9 +36,94 @@ enum ScanPathPreset {
             name: "Docker Container Data"
         ),
         CommonPreset(
+            id: UUID(uuidString: "7F39C716-6D31-4A7A-BD44-2CA1B5809F95")!,
+            pathBuilder: { _, home in home.appendingPathComponent("Library/Group Containers/group.com.docker", isDirectory: true) },
+            name: "Docker Group Container"
+        ),
+        CommonPreset(
             id: UUID(uuidString: "198E6B80-B3A9-4C09-9866-8A27C67D6D66")!,
             pathBuilder: { base, _ in base.appendingPathComponent(".cache", isDirectory: true) },
             name: "Base .cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "9E2CD56B-B549-4DDE-A8B2-68CD6F13E5F4")!,
+            pathBuilder: { _, home in home.appendingPathComponent("Library/Developer/Xcode/Archives", isDirectory: true) },
+            name: "Xcode Archives"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "2D5A2556-F359-4CE2-B45C-A7A26B390A84")!,
+            pathBuilder: { _, home in home.appendingPathComponent("Library/Developer/Xcode/iOS DeviceSupport", isDirectory: true) },
+            name: "Xcode DeviceSupport"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "2A1BA40B-0F7A-4E1F-A4A6-3DF86E188D25")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".npm", isDirectory: true) },
+            name: "npm Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "4A2B3374-7AE4-424E-B632-7BB4B6811D78")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".pnpm-store", isDirectory: true) },
+            name: "pnpm Store"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "7AC5096A-B8A6-4A4C-BD76-9E5AD6A00A42")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".yarn", isDirectory: true) },
+            name: "Yarn Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "AF2714DE-B2E2-48EC-9F2E-6A4A4AE01361")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".bun/install/cache", isDirectory: true) },
+            name: "Bun Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "3F3A9012-6838-49C8-900E-713E4D2FD993")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".gradle/caches", isDirectory: true) },
+            name: "Gradle Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "0A368D0F-EEA5-4AF9-A9CF-CE3A812D8F0B")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".m2/repository", isDirectory: true) },
+            name: "Maven Repo"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "A0D7E6DA-4A20-4AA7-8C1C-D095F4D410C5")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".cargo/registry", isDirectory: true) },
+            name: "Cargo Registry"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "7347C287-8A74-4540-8A51-35F8A1092D3F")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".cargo/git", isDirectory: true) },
+            name: "Cargo Git Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "0F43E7F0-706F-4666-BF0B-4CEB37295DFB")!,
+            pathBuilder: { _, home in home.appendingPathComponent("go/pkg/mod", isDirectory: true) },
+            name: "Go Module Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "68BC01E1-0311-42C5-996E-C20A332F8562")!,
+            pathBuilder: { _, home in home.appendingPathComponent(".cache/pip", isDirectory: true) },
+            name: "pip Cache"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "64A3F89F-3D06-4EC9-8E77-8B4A69C91E5A")!,
+            pathBuilder: { _, _ in URL(fileURLWithPath: "/Library/Caches", isDirectory: true) },
+            name: "System Library Caches"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "8A4A7A9F-B1DD-4689-AF09-18E2B3AEBF97")!,
+            pathBuilder: { _, _ in URL(fileURLWithPath: "/private/var/folders", isDirectory: true) },
+            name: "System Temp Folders"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "2A53A90E-3E4B-4F77-9A2E-91CB7A97D6AA")!,
+            pathBuilder: { _, _ in URL(fileURLWithPath: "/private/var/tmp", isDirectory: true) },
+            name: "System Temp"
+        ),
+        CommonPreset(
+            id: UUID(uuidString: "6E4D6C4D-19CC-4A01-A5E8-4B78A4A1F8EE")!,
+            pathBuilder: { _, _ in URL(fileURLWithPath: "/private/var/log", isDirectory: true) },
+            name: "System Logs"
         )
     ]
 
